c5c5e2927a2d939dcac565f5105aa9353bdd2edb
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-01-31 19:57:11 +0100

    Kreiranje binova potrebnih za rad i resavanje cors-a

---
+++ src/main/java/com/jogger/rs/config/AppConfig.java
@@ -0,0 +1,32 @@
+package com.jogger.rs.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PasswordEncoder bCrypt() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
