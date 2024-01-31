c67e5e0071c6e9c17fe7ca032f15730a91dc580b
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-01-31 19:58:41 +0100

    Dodat job da se sesije brisu na 5 dana

--- src/main/java/com/jogger/rs/config/AppConfig.java
+++ src/main/java/com/jogger/rs/config/AppConfig.java
@@ -1,19 +1,36 @@
 package com.jogger.rs.config;
 
+import com.jogger.rs.auth.SessionManager;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableScheduling
 @EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
+
+    private SessionManager sessionManager;
+
+    @Autowired
+    public AppConfig(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.DAYS)
+    private void clearAllUserSessions() {
+        sessionManager.clearAllSessions();
+    }
 
     @Bean
     public PasswordEncoder bCrypt() {
