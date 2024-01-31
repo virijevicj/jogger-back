10084dced6be79e3f917e0e6fbd29db7bbb85416
virijevicj <89025058+virijevicj@users.noreply.github.com>
2024-01-31 17:58:28 +0100

    Kreiran projekat i sredjen pom.xml

---
+++ src/main/java/com/jogger/rs/JoggerApplication.java
@@ -0,0 +1,13 @@
+package com.jogger.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jogger/rs/JoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogger.rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
