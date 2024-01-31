a790be29125ec76c73992ae24f1029dd26621da2
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-01-31 20:01:24 +0100

    Dto za rad sa user-om

---
+++ src/main/java/com/jogger/rs/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.jogger.rs.dto;
+
+import com.jogger.rs.user.User;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Integer keyUser;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Boolean active;
+    List<Integer> rolesKeys;
+
+}
