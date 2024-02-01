b3985fa9fd02385fd649f865e71ea5ff77f9cf59
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-01 15:29:26 +0100

    izmena zbog put zahteva (mora eksplicitno da se naglasi jer se ne podrazumeva)

--- src/main/java/com/jogger/rs/config/AppConfig.java
+++ src/main/java/com/jogger/rs/config/AppConfig.java
@@ -44,6 +44,7 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
