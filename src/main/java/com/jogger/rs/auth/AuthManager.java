7fb69c888bfd2a01d215445f8f6f71f5b04e830c
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-01 13:31:30 +0100

    Bean cija je uloga da vrsi autorizaciju pristiglih zahteva, odnosno da li neki korisnik ima pravo da pristupi nekom resorsu na nekoj putanji. Pravila ce biti dodavana kako se budu definisali api-ji.

---
+++ src/main/java/com/jogger/rs/auth/AuthManager.java
@@ -0,0 +1,68 @@
+package com.jogger.rs.auth;
+
+import com.jogger.rs.labels.ErrorMessage;
+import com.jogger.rs.labels.RequestMappingPrefix;
+import com.jogger.rs.role.Role;
+import com.jogger.rs.role.RoleName;
+import com.jogger.rs.user.UserSession;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.security.sasl.AuthenticationException;
+import java.util.*;
+
+@CommonsLog
+@Component
+public class AuthManager {
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+    private HashMap<String, List<RoleName>> authRules = new HashMap<>();
+    private SessionManager sessionManager;
+
+    @Autowired
+    public AuthManager(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+        addAuthRules();
+    }
+
+    private void addAuthRules() {
+        // ovde cemo da definisemo pravila koji request mora da ima koju ulogu
+    }
+
+    public Boolean auth(HttpServletRequest request) throws AuthenticationException {
+        if (authRules == null) addAuthRules();
+        if ((contextPath + RequestMappingPrefix.AUTH + "/login").equals(request.getRequestURI())) return true; // login dozvoljavamo svima
+        String token = request.getHeader("Token");
+        if (!StringUtils.hasText(token))
+            throw new AuthenticationException(ErrorMessage.NO_TOOKEN_IN_HEADER);
+        UserSession user = sessionManager.getUserFromSession(token).orElseThrow(() ->
+                new AuthenticationException(ErrorMessage.NO_TOKEN_FOUND + token));
+        String method = request.getMethod();
+        String requestMapping = extractRequestMappingFromRequest(request);
+        List<RoleName> roles = authRules.get(method + "-" + requestMapping);
+        if (roles == null) {
+            log.error(String.format(ErrorMessage.NO_RULES_ADDED, requestMapping, method, request.getRequestURI()));
+            return false;
+        }
+        if (roles.isEmpty()) return true; // prazna lista znaci da je svima dozvolje da pristupi resursu, bez obzira na ulogu
+        return checkIfUserHasNecessaryRoles(user, roles);
+    }
+
+    private String extractRequestMappingFromRequest(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String tmp = uri.substring(contextPath.length());
+        return tmp.indexOf("/", 1) == -1 ? tmp : tmp.substring(0, tmp.indexOf("/", 1));
+    }
+
+    private Boolean checkIfUserHasNecessaryRoles(UserSession user, List<RoleName> roles) {
+        for (Role role : user.getRoles()) {
+            if (roles.contains(role.getName())) return true;
+        }
+        return false;
+    }
+}
