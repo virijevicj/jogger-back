1b1cd05b014704101845ba04eb3e0059a4a9233c
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-05 15:13:33 +0100

    izmenjeni nazivi resursa koji otrivaju o kojem resursu se radi (requestMapping)

--- src/main/java/com/jogger/rs/labels/RequestMappingPrefix.java
+++ src/main/java/com/jogger/rs/labels/RequestMappingPrefix.java
@@ -5,7 +5,7 @@ public interface RequestMappingPrefix {
     public final static String AUTH = "/auth";
     public final static String USER = "/users";
     public final static String LEARNING_MATERIAL = "/lm";
-    public final static String LEARNING_MATERIAL_ENTITIES = "/lm/entities";
-    public final static String LEARNING_MATERIAL_COMMENT = "/lm/comment";
+    public final static String LEARNING_MATERIAL_ENTITIES = "/lm-entities";
+    public final static String LEARNING_MATERIAL_COMMENT = "/lm-comments";
 
 }
