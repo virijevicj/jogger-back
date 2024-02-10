ada90c4b287f38551cc18fa9366c9eb968033bac
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 00:47:25 +0100

    javadoc klasa AuthController

--- src/main/java/com/jogger/rs/auth/AuthController.java
+++ src/main/java/com/jogger/rs/auth/AuthController.java
@@ -17,14 +17,37 @@ import org.springframework.web.bind.annotation.*;
 import javax.security.sasl.AuthenticationException;
 import java.util.Optional;
 
+/**
+ * Kontroler koji prihvata zahteve vezane za autentifikaciju korisnika, odnosno login i logout funkcionalnosti sistema.
+ *
+ * @author Jovan Virijevic
+ */
 @RestController
 @RequestMapping(RequestMappingPrefix.AUTH)
 public class AuthController {
 
+    /**
+     * Servis koji se poziva da izvrsi login i logout
+     */
     private UserServiceInterface userService;
+
+    /**
+     * Servis koji se poziva da kreira odgovor na korisnicki zahtev
+     */
     private ResponseFactory responseFactory;
+
+    /**
+     * Servis koji se poziva da izvrsi autorizaciju korisnickog zahteva
+     */
     private AuthManager authManager;
 
+    /**
+     * Javni konstruktor
+     *
+     * @param userService servis koji se poziva da izvrsi login i logout
+     * @param responseFactory servis koji se poziva da kreira odgovor na korisnicki zahtev
+     * @param authManager servis koji se poziva da izvrsi autorizaciju korisnickog zahteva
+     */
     @Autowired
     public AuthController(UserServiceInterface userService, ResponseFactory responseFactory, AuthManager authManager) {
         this.userService = userService;
@@ -32,6 +55,14 @@ public class AuthController {
         this.authManager = authManager;
     }
 
+    /**
+     * Metoda koja prihvata korisnicki zahtev da se izvrsi login
+     *
+     * @param request zahtev koji salje klijentska strana
+     * @param loginRequestDto objekat koji sadrzi username i password potreban za login
+     * @return ResponseObject koji se puni podacima u zavisnosti da li je uspesno izvrsen login ili ne
+     * @throws AuthenticationException ako korisnik nema pravo da pristupi datoj putanji
+     */
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     private @ResponseBody ResponseEntity<Object> login(HttpServletRequest request, @RequestBody LoginRequestDto loginRequestDto) throws AuthenticationException {
         if (!authManager.auth(request))
@@ -41,6 +72,13 @@ public class AuthController {
         return responseFactory.ok(SuccessMessage.LOGIN_SUCCESS, responseDto);
     }
 
+    /**
+     * Metoda koja prihvata korisnicki zahtev da se izvrsi logout
+     *
+     * @param request zahtev koji salje klijentska strana
+     * @return ResponseObject koji se puni podacima u zavisnosti da li je uspesno izvrsen logout ili ne
+     * @throws AuthenticationException ako korisnik nema pravo da pristupi datoj putanji
+     */
     @PostMapping(value = "/logout", produces = MediaType.APPLICATION_JSON_VALUE)
     private @ResponseBody ResponseEntity<Object> logout(HttpServletRequest request) throws AuthenticationException {
         if (!authManager.auth(request))
