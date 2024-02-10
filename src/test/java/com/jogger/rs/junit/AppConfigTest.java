a985e0693ae508121e441517a51ceffd9f3af787
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-10 18:57:48 +0100

    dodati testovi za klasu AppConfig

---
+++ src/test/java/com/jogger/rs/junit/AppConfigTest.java
@@ -0,0 +1,39 @@
+package com.jogger.rs.junit;
+
+import com.jogger.rs.auth.SessionManager;
+import com.jogger.rs.config.AppConfig;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppConfigTest {
+
+    AppConfig appConfig;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        appConfig = new AppConfig(new SessionManager());
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        appConfig = null;
+    }
+
+    @Test
+    @DisplayName("AppConfigTest - kreiranje BCryptPasswordEncoder objekta")
+    void bCrypt() {
+        assertInstanceOf(BCryptPasswordEncoder.class, appConfig.bCrypt());
+    }
+
+    @Test
+    @DisplayName("AppConfigTest - kreiranje BCryptPasswordEncoder ModelMapper")
+    void modelMapper() {
+        assertInstanceOf(ModelMapper.class, appConfig.modelMapper());
+    }
+}
