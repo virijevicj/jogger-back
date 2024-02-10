aae16ab27869a6098f8e8b3556877403c0d3a03e
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-10 23:44:20 +0100

    javadoc klasa User

--- src/main/java/com/jogger/rs/user/User.java
+++ src/main/java/com/jogger/rs/user/User.java
@@ -1,6 +1,5 @@
 package com.jogger.rs.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jogger.rs.role.Role;
 import jakarta.persistence.*;
@@ -8,6 +7,14 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Predstavlja korisnika sistema.
+ *
+ * Korisnik ima id, username, lozinku, ime, prezime, email da li je aktivan ili ne, kao i listu uloga koje ima u sistemu.
+ *
+ * @author Jovan Virijevic
+ *
+ */
 @Data
 @Entity
 @Table(name = "user", indexes = {
@@ -15,31 +22,55 @@ import java.util.List;
 })
 public class User {
 
+    /**
+     * Jedinstveni identifikator korisnika
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "key_user", nullable = false, unique = true, updatable = false)
     @JsonProperty(access= JsonProperty.Access.READ_ONLY)
     private Integer keyUser;
 
+    /**
+     * Username koji korisnik koristi da se uloguje na sistem
+     */
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
+    /**
+     * Lozinka koju korisnik koristi da se uloguje na sistem
+     */
     @JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", nullable = false)
     private String password;
 
+    /**
+     * Ime korisnika
+     */
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    /**
+     * Prezime korisnika
+     */
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    /**
+     * Email korisnika
+     */
     @Column(name = "email", nullable = false)
     private String email;
 
+    /**
+     * Da li je korisnik aktivan ili ne (obrisan)
+     */
     @Column(name = "active", nullable = false, columnDefinition = "boolean default true")
     private Boolean active;
 
+    /**
+     * Lista uloga koje korisnik ima
+     */
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
