5c92641d59a3516a7be94ec477d507861f3797f2
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:19:26 +0100

    javadoc klasa NewCommentDto

--- src/main/java/com/jogger/rs/dto/NewCommentDto.java
+++ src/main/java/com/jogger/rs/dto/NewCommentDto.java
@@ -2,9 +2,29 @@ package com.jogger.rs.dto;
 
 import lombok.Data;
 
+/**
+ * Dto objekat zaduzen za komunikaciju klijentske i serverske strane aplikacije.
+ *
+ * Sadrzi jedinstveni identifikator materijala za ucenje, ocenu i komentar na materijal za ucenje.
+ * Koristi se kada se dodaje nov komentar za neki materijal za ucenje.
+ *
+ * @author Jovan Virijevic
+ */
 @Data
 public class NewCommentDto {
+
+    /**
+     * Jedinstveni identifikator materijala za ucenje
+     */
     private Integer keyLearningMaterial;
+
+    /**
+     * Ocena koja se dodeljuje nekom materijalu za ucenje.
+     */
     private Integer grade;
+
+    /**
+     * Komentar koji se dodeljuje nekom materijalu za ucenje.
+     */
     private String text;
 }
