6636007b8d05bb64de4a60c49e443c43c45b0841
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:14:21 +0100

    javadoc klasa LMEntitiesDto

--- src/main/java/com/jogger/rs/dto/LMEntitiesDto.java
+++ src/main/java/com/jogger/rs/dto/LMEntitiesDto.java
@@ -6,12 +6,39 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Dto objekat zaduzen za komunikaciju klijentske i serverske strane aplikacije.
+ *
+ * Sadrzi sve entitete vezane za materijale za ucenje koji su potrebni za pretragu materijala za ucenje.
+ *
+ * @author Jovan Virijevic
+ */
 @Data
 @Builder
 public class LMEntitiesDto {
+
+    /**
+     * Lista oblasti materijala za ucenje.
+     */
     List<AreaName> areas;
+
+    /**
+     * Lista tipova sadrzaja materijala za ucenje.
+     */
     List<ContentTypeName> contentTypes;
+
+    /**
+     * Lista nivoa materijala za ucenje.
+     */
     List<LevelName> levels;
+
+    /**
+     * Lista platformi materijala za ucenje.
+     */
     List<PlatformName> platforms;
+
+    /**
+     * Lista tehnologija materijala za ucenje.
+     */
     List<TechnologyName> technologies;
 }
