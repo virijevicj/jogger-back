8140e3e4196fa6eca0e65cb2ad41bb73c58a1d31
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:10:57 +0100

    javadoc klasa AppConfig

--- src/main/java/com/jogger/rs/config/AppConfig.java
+++ src/main/java/com/jogger/rs/config/AppConfig.java
@@ -16,34 +16,65 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Globalna konfiguracija aplikacije plus kreiranje spring binova preko metoda
+ *
+ * @author Jovan Virijevic
+ */
 @Configuration
 @EnableScheduling
 @EnableWebMvc
 @EnableAsync
 public class AppConfig implements WebMvcConfigurer {
 
+    /**
+     * Servis koji vodi racuna o korisnickim sesijama
+     */
     private SessionManager sessionManager;
 
+    /**
+     * Javni konstruktor
+     *
+     * @param sessionManager servis koji vodi racuna o korisnickim sesijama
+     */
     @Autowired
     public AppConfig(SessionManager sessionManager) {
         this.sessionManager = sessionManager;
     }
 
+    /**
+     * Job koji se okida na svakih 5 dana i prazni mapu korisnickih sesija
+     */
     @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.DAYS)
     private void clearAllUserSessions() {
         sessionManager.clearAllSessions();
     }
 
+    /**
+     * Metoda koja kreira bean BCryptPasswordEncoder
+     *
+     * @return BCryptPasswordEncoder
+     */
     @Bean
     public PasswordEncoder bCrypt() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Metoda koja kreira bean ModelMapper
+     *
+     * @return ModelMapper
+     */
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
+    /**
+     * Metoda koja definise koje metode smeju da dele svoje resurse
+     *
+     * @param registry konfigurisan CorsRegistry objekat sa metodama kojima je dozvoljeno da dele resurse aplikacije
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
