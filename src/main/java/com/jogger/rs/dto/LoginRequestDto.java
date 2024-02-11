acf6eca550a9e652b5cde1bc383cd328f1e89e96
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:15:47 +0100

    javadoc klasa LoginRequestDto

--- src/main/java/com/jogger/rs/dto/LoginRequestDto.java
+++ src/main/java/com/jogger/rs/dto/LoginRequestDto.java
@@ -2,9 +2,24 @@ package com.jogger.rs.dto;
 
 import lombok.Data;
 
+/**
+ * Dto objekat zaduzen za komunikaciju klijentske i serverske strane aplikacije.
+ *
+ * Sadrzi username i password potreban za login korisnika na sistem.
+ *
+ * @author Jovan Virijevic
+ */
 @Data
 public class LoginRequestDto {
+
+    /**
+     * Username korisnika potrebna za login.
+     */
     private String username;
+
+    /**
+     * Lozinka korisnika potrebna za login.
+     */
     private String password;
 
 }
