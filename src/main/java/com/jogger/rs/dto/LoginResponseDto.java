fadf2ba8b2b43512b2e4a22571a5dfaa7fc2bba6
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:17:25 +0100

    javadoc klasa LoginResponseDto

--- src/main/java/com/jogger/rs/dto/LoginResponseDto.java
+++ src/main/java/com/jogger/rs/dto/LoginResponseDto.java
@@ -5,9 +5,24 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Dto objekat zaduzen za komunikaciju klijentske i serverske strane aplikacije.
+ *
+ * Sadrzi token i korisnicke uloge u sistemu i salje se nakon uspesnog login-a korisnika na sistem.
+ *
+ * @author Jovan Virijevic
+ */
 @Data
 @Builder
 public class LoginResponseDto {
+
+    /**
+     * Jedinistveni token koji se dodeljuje korisniku nakon login-a.
+     */
     private String token;
+
+    /**
+     * Lista uloga koje korisnik ima u sistemu.
+     */
     private List<String> roles;
 }
