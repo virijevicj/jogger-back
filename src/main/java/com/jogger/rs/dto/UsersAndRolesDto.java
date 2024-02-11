7cb4441a46001b99cf0557254e5e97132e0ea581
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:29:55 +0100

    javadoc klasa UsersAndRolesDto

--- src/main/java/com/jogger/rs/dto/UsersAndRolesDto.java
+++ src/main/java/com/jogger/rs/dto/UsersAndRolesDto.java
@@ -7,9 +7,25 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * Dto objekat zaduzen za komunikaciju klijentske i serverske strane aplikacije.
+ *
+ * Sadrzi listu korisnika sistema kao i uloga koje postoje u sistemu.
+ * Koristi se kada se pronalaze svi korisnici sistema.
+ *
+ * @author Jovan Virijevic
+ */
 @Data
 @Builder
 public class UsersAndRolesDto {
+
+    /**
+     * Lista korisnika sistema.
+     */
     List<User> users;
+
+    /**
+     * Lista uloga koje postoje u sistemu.
+     */
     List<Role> roles;
 }
