c8558008cda9c687d4190372867445e6ff4c2947
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 01:24:33 +0100

    javadoc klasa StandardResponseDto

--- src/main/java/com/jogger/rs/dto/StandardResponseDto.java
+++ src/main/java/com/jogger/rs/dto/StandardResponseDto.java
@@ -4,14 +4,39 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+
+/**
+ * Dto objekat zaduzen za komunikaciju klijentske i serverske strane aplikacije.
+ *
+ * Sadrzi vreme kad je odgovor kreiran, statusni kod, poruku i podatke koje je sistem generisao za neki korisnicki zahtev.
+ * Koristi se uvek kao objekat koji se salje na korisnicki zahtev.
+ *
+ * @author Jovan Virijevic
+ */
+
 @Data
 @Builder
 public class StandardResponseDto {
 
+    /**
+     * Vreme kreiranja odgovora za dati korisnicki zahtev.
+     */
     @Builder.Default
     private final LocalDateTime time = LocalDateTime.now();
+
+    /**
+     * Statusni kod korisnickog zahteva.
+     */
     private Integer statusCode;
+
+    /**
+     * Poruka za dati korisnicki zahtev.
+     */
     private String message;
+
+    /**
+     * Podaci za dati korisnicki zahtev.
+     */
     private Object data;
 
 }
