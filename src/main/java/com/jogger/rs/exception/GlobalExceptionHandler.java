59196b6e66334db82c85724ddeb2f334968a9deb
Jovan Virijevic <jovanvirijevic2@gmail.com>
2024-02-11 22:44:35 +0100

    javadoc za klasu GlobalExceptionHandler

--- src/main/java/com/jogger/rs/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jogger/rs/exception/GlobalExceptionHandler.java
@@ -12,33 +12,64 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.security.sasl.AuthenticationException;
 import java.util.NoSuchElementException;
 
+/**
+ * Klasa koja je zaduzena za hvatanje izuzetaka nastalih u aplikaciji i kreiranje odgovora na osnovu tih izuzetaka.
+ *
+ * @author Jovan Virijevic
+ */
 @CommonsLog
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+    /**
+     * Servis koja je zaduzen za kreiranje odgovora korisniku.
+     */
     private ResponseFactory responseFactory;
 
+    /**
+     * Javni konstruktor.
+     *
+     * @param responseFactory servis koja je zaduzen za kreiranje odgovora korisniku.
+     */
     @Autowired
     public GlobalExceptionHandler(ResponseFactory responseFactory) {
         this.responseFactory = responseFactory;
     }
 
+    /**
+     * Metoda koja hvata izuzetak IllegalArgumentException i vraca odgovarajuci response.
+     * @param exception uhvacen izuzetak.
+     * @return ResponseEntity<Object>
+     */
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public @ResponseBody ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException exception) {
         return responseFactory.badRequest(exception.getMessage());
     }
 
+    /**
+     * Metoda koja hvata izuzetak NoSuchElementException i vraca odgovarajuci response.
+     * @param exception uhvacen izuzetak.
+     * @return ResponseEntity<Object>
+     */
     @ExceptionHandler(value = {NoSuchElementException.class})
     public @ResponseBody ResponseEntity<Object>  handleNoSuchElementException(NoSuchElementException exception) {
         return responseFactory.notFound(exception.getMessage());
     }
 
+    /**
+     * Metoda koja hvata izuzetak AuthenticationException i vraca odgovarajuci response.
+     * @param exception uhvacen izuzetak.
+     * @return ResponseEntity<Object>
+     */
     @ExceptionHandler(value = {AuthenticationException.class})
     public @ResponseBody ResponseEntity<Object>  handleAuthenticationException(AuthenticationException exception) {
         // NO_TOKEN_FOUND - > UNAUTHORIZED
         return responseFactory.unauthorized(exception.getMessage());
     }
-
+    /**
+     * Metoda koja hvata izuzetak RuntimeException i vraca odgovarajuci response.
+     * @param exception uhvacen izuzetak.
+     * @return ResponseEntity<Object>
+     */
     @ExceptionHandler(value = {RuntimeException.class})
     public @ResponseBody ResponseEntity<Object> handleRunTimeException(RuntimeException exception) {
         log.error(exception.getMessage());
